549debfc82f5df2e021a7a81f08c529b5984daa4
James Gifford <jgifford@acorns.com>
2020-05-29 16:52:54 -0700

    Fix laps

--- app.js
+++ app.js
@@ -5,38 +5,50 @@ import styled from 'styled-components'
 const MOUNT_NODE = document.getElementById('app')
 
 const Layout = styled.div`
-  display: grid
-  text-align: center
-  font-family: sans-serif
+  display: grid;
+  text-align: center;
+  font-family: sans-serif;
 `
 
 const TimerDisplay = styled.div`
-  padding: 10px
-  font-family: 'VT323', sans-serif
-  font-size: 60px
+  padding: 10px;
+  font-family: 'VT323', sans-serif;
+  font-size: 60px;
 `
 
 const TimerControls = styled.div`
-  display: inline-block
+  display: inline-block;
 `
 
 const TimerButton = styled.button`
-  font-size: 20px
-  margin-right: 5px
+  font-size: 20px;
+  margin-right: 5px;
 `
 
 const LapList = styled.div`
-  height: 600px
-  padding: 15px
-  text-align: right
-  overflow-y: scroll
+  height: 600px;
+  padding: 15px;
+  text-align: right;
+  overflow-y: scroll;
 `
 
 const Lap = styled.div`
-  font-size: 16px
-  padding-bottom: 3px
+  font-size: 16px;
+  padding-bottom: 3px;
 `
 
+const fmtTime = (duration) => {
+  let millis = parseInt((duration % 1000) / 10)
+    , seconds = parseInt((duration / 1000) % 60)
+    , minutes = parseInt((duration / (1000 * 60)) % 60)
+    , hours = parseInt((duration / (1000 * 60 * 60)) % 24)
+
+  hours = (hours < 10) ? "0" + hours : hours
+  minutes = (minutes < 10) ? "0" + minutes : minutes
+  seconds = (seconds < 10) ? "0" + seconds : seconds
+
+  return hours + ":" + minutes + ":" + seconds + "." + `${millis}`.padStart(2, 0)
+}
 
 class Timer {
   constructor() {
@@ -65,24 +77,15 @@ class Timer {
   }
 
   getTime() {
-    return this.fmtTime(this.time)
+    return this.time
+  }
+
+  getElaspedSince(t) {
+    return this.time - t
   }
 
   isPaused() {
     return this.paused
-  }
-
-  fmtTime(duration) {
-    let millis = parseInt((duration % 1000) / 10)
-      , seconds = parseInt((duration / 1000) % 60)
-      , minutes = parseInt((duration / (1000 * 60)) % 60)
-      , hours = parseInt((duration / (1000 * 60 * 60)) % 24)
-
-    hours = (hours < 10) ? "0" + hours : hours
-    minutes = (minutes < 10) ? "0" + minutes : minutes
-    seconds = (seconds < 10) ? "0" + seconds : seconds
-
-    return hours + ":" + minutes + ":" + seconds + "." + `${millis}`.padStart(2, 0)
   }
 }
 
@@ -109,7 +112,15 @@ const App = ({ timer }) => {
 
   const handleLap = () => {
     if (timer.isPaused()) return
-    setLaps([...laps, timer.getTime()])
+
+    const nextTotalDuration = timer.getTime()
+
+    if (laps.length) {
+      const [prevTotalDuration] = laps.slice(-1)[0]
+      setLaps([...laps, [nextTotalDuration, nextTotalDuration - prevTotalDuration]])
+    } else {
+      setLaps([[nextTotalDuration, nextTotalDuration]])
+    }
   }
 
   const animate = () => {
@@ -124,7 +135,7 @@ const App = ({ timer }) => {
 
   return (
     <Layout>
-      <TimerDisplay>{time}</TimerDisplay>
+      <TimerDisplay>{fmtTime(time)}</TimerDisplay>
       <TimerControls>
         {timer.isPaused() ?
           <TimerButton onClick={handleStart}>Start</TimerButton> :
@@ -136,8 +147,8 @@ const App = ({ timer }) => {
       </TimerControls>
 
       <LapList>
-        {laps.map((lap, i) => (
-          <Lap key={i}>Lap {i + 1}: {lap}</Lap>
+        {laps.map(([_, lapDuration], i) => (
+          <Lap key={i}>Lap {i + 1}: {fmtTime(lapDuration)}</Lap>
         ))}
       </LapList>
     </Layout>
